5b7d570fa9b54f522c470a0bb2b66ddc63c1cdca
= <=>
2026-09-17 10:11:30 +0200

    dziala liczenie i wyswietlanie wyniku bez zdjec

--- app/src/main/java/com/example/kostki/MainActivity.java
+++ app/src/main/java/com/example/kostki/MainActivity.java
@@ -34,49 +34,53 @@ public class MainActivity extends AppCompatActivity {
         kostka5 = findViewById(R.id.imageView5);
         textViewWynik = findViewById(R.id.textViewWynik);
 
-//        ArrayList<ImageView> obrazki = new ArrayList<>();
-//        obrazki.add(kostka1);
-//        obrazki.add(kostka2);
-//        obrazki.add(kostka3);
-//        obrazki.add(kostka4);
-//        obrazki.add(kostka5);
+        int[]listaZdjec = new int[]{
+                R.drawable.k1, R.drawable.k2, R.drawable.k3, R.drawable.k4, R.drawable.k5
+        };
+
+        ArrayList<ImageView> obrazki = new ArrayList<>();
+        obrazki.add(kostka1);
+        obrazki.add(kostka2);
+        obrazki.add(kostka3);
+        obrazki.add(kostka4);
+        obrazki.add(kostka5);
 
         przyciskRzuc.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        System.out.println(losujKostke(5));
+                        System.out.println(losujKostke(5, textViewWynik, listaZdjec, obrazki));
                     }
                 }
         );
 
     }
-    public static int losujKostke(int ileKostek) {
+    public static int losujKostke(int ileKostek, TextView textViewWynik, int[]listaZdjec, ArrayList<ImageView> obrazki) {
         Random random = new Random();
 
         int[] lista = new int[ileKostek];
 
         for (int i = 1; i <= ileKostek; i++) {
-            int wynikRzutu = random.nextInt(6);
+            int wynikRzutu = random.nextInt(6)+1;
             System.out.println("Kostka " + i + ": " + wynikRzutu);
             lista[i - 1] = wynikRzutu;
-
+            //obrazki.get(i - 1).setImageResource(listaZdjec[wynikRzutu-1]);
         }
-        return obliczPunkty(lista);
+        return obliczPunkty(lista, textViewWynik);
     }
 
-    public static int obliczPunkty(int[] lista) {
+    public static int obliczPunkty(int[] lista, TextView textViewWynik) {
         int[] indeksy = new int[]{0, 0, 0, 0, 0, 0};
         int wynik2 = 0;
         for (int i = 0; i < lista.length; i++) {
-            indeksy[lista[i] - 1]++;
+            indeksy[lista[i]-1]++;
         }
         for (int i = 0; i < 6; i++) {
             if (indeksy[i] >= 2) {
                 wynik2 += (i + 1) * indeksy[i];
             }
         }
-
+        textViewWynik.setText("Wynik tego losowania: "+wynik2);
         return wynik2;
     }
 }
